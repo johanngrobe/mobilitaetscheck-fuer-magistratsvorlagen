be6a8cd2c3130be002baf55510d5b9935c5f0797
Johann Grobe <j.grobe@namowo.de>
2026-08-25 20:36:06 +0200

    Fix casing: rename to textblock.js

--- frontend/src/utils/schemas/textBlock.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup'
-
-// Validation schema
-export const schema = yup.object({
-  name: yup.string().required('Angabe ist erforderlich'),
-  tagIds: yup.array().of(yup.number()).nullable(true),
-  gemeindespezifisch: yup.boolean().default(false)
-})
